df5786e10206ed670319f4ca2b4164553a1e154f
AntonioHugo21 <antoniohugo.pierre@gmail.com>
2024-09-18 15:02:06 -0300

    anos bissextos

--- atividade17.py
+++ atividade17.py
@@ -2,7 +2,7 @@
 
 ano = int(input("digite o ano"))
 
-if(ano%4==0 and ano%100!=0):
+if(ano%4==0 and ano%100!=0) or (ano%400 == 0):
     print("O ano é bissexto")
 else:
     print("O ano não é bissexto")
